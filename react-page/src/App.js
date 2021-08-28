779e94202cba3962d6b29f39612d4a4093549500
Purva Pathak <pathakpurva1998@gmail.com>
2021-08-28 12:51:34 +0530

    reactpage

---
+++ react-page/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+import Programcard from "./Components/Programcard";
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1 class="title"> Internship program </h1>
+      
+      <div className="programCardContainer" >
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+      <Programcard/>
+     
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-page/src/Components/Programcard.js
@@ -0,0 +1,16 @@
+import "./Programcard.css"
+export default function Programcard() {
+    return(
+        <div className="cards">
+             <img src="./image/img.jpeg" alt="imag" />
+             <h2 class="depatment">UI/UX Designing</h2>
+             <button class="Button1">
+                 Button
+             </button>
+             <button class="Button2">
+                 Button
+             </button>
+        </div>
+    )
+}
+
